b2509fea921460964df4a44d7cea0b19a9070e95
AStefankiv <stefankif35@gmail.com>
2024-05-03 16:09:22 -0600

    Remove css

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-import styles from './contact.module.css'; // Importing our CSS module
-
 export default function Contact() {
   return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <h2 className={styles.heading}>Contact Information:</h2>
+    <div className="flex justify-between mx-4">
+      <div className="w-1/2 p-4">
+        <h2 className="text-2xl font-bold">Contact Information:</h2>
         <br />
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2508.7461286638427!2d-114.0993724585357!3d51.03930944490807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53716fd853c4bdd5%3A0xfd2962f14ddacfc7!2s1533%2015%20Ave%20SW%2C%20Calgary%2C%20AB%20T3C%200Y1!5e0!3m2!1sen!2sca!4v1714694012344!5m2!1sen!2sca"
@@ -21,9 +19,9 @@ export default function Contact() {
         <p>+1 234 567 890</p>
         <p>email@example.com</p>
       </div>
-      <div className={styles.right}>
-        <form style={{ display: "flex", flexDirection: "column", width: "100%" }}>
-          <h2 className={styles.heading}>Contact us:</h2>
+      <div className="w-1/2 p-4">
+        <form className="flex flex-col" style={{ width: "100%" }}>
+          <h2 className="text-2xl font-bold mb-4">Contact us:</h2>
           <br />
           <label htmlFor="name">Name:</label>
           <input type="text" id="name" name="name" required />
@@ -33,7 +31,7 @@ export default function Contact() {
           <input type="email" id="email" name="email" required />
           <label htmlFor="message">Message:</label>
           <textarea id="message" name="message" rows="6" required></textarea>
-          <button type="submit" className={styles.submitButton}>Send us</button>
+          <button type="submit" className="mx-auto bg-orange-500 text-white py-2 px-2 rounded hover:bg-orange-600 transition duration-300 mt-4 font-bold">Send us</button>
         </form>
       </div>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import styles from "./Footer.module.css";
+// import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className="{styles.content}">
+    <footer className="fixed bottom-0 w-full bg-gray-900 text-white py-2 text-center">
+      <div className="inline">
         <p>&copy; {new Date().getFullYear()} Intro Foods. All rights reserved.</p>
         {/* <ul>
           <li>About Us</li>
