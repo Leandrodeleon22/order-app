9dc917b2f0c646d87d0e59c7a75e7a5aa3bef111
Leandrodeleon22 <leandrodeleon22@gmail.com>
2024-05-04 16:00:34 -0400

    fix code in home

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { MdDeleteOutline } from "react-icons/md";
-import { IoIosClose } from "react-icons/io";
 import Counter from "./Counter";
 
 const Orders = () => {
@@ -10,12 +9,9 @@ const Orders = () => {
         <h1 className="font-medium">My Orders</h1>
         <MdDeleteOutline />
       </div>
-      <div className="bg-slate-100 rounded-md p-3">
-        <div className="flex justify-between items-center">
-          <p>Toronto Burger</p>
-          <IoIosClose className="text-2xl cursor-pointer" />
-        </div>
-        <p>Delicious toronto burger</p>
+      <div className="bg-slate-100 p-2 rounded-md">
+        <p>Toronto Burger</p>
+        <p>Delicious toronto burer</p>
         <div className="flex justify-between">
           <span>$50</span>
           <Counter />
