f29c4a6067c4bfedb621a9e46b60ba45aa8f831a
Leandrodeleon22 <leandrodeleon22@gmail.com>
2024-05-04 16:14:35 -0400

    fix counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,14 +4,12 @@ import { FaMinus } from "react-icons/fa";
 const Counter = () => {
   return (
     <div className="flex">
-      <button className="bg-gray-200 px-2 rounded-[5px]"
-        <FaMinus />
-      </button>
-
-      <div className="w-7 bg-slate-50 text-center">1</div>
-
       <button className="bg-gray-200 px-2 rounded-[5px]">
         <FaPlus />
+      </button>
+      <div>1</div>
+      <button className="bg-gray-200 px-2 rounded-[5px]">
+        <FaMinus />
       </button>
     </div>
   );
