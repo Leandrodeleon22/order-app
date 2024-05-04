a0fac60612a4de4f60a0b20ad2adf642df55781c
Leandrodeleon22 <leandrodeleon22@gmail.com>
2024-05-04 14:17:05 -0400

    edit order component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Products = () => {
-  return <div className="bg-green-300 mx-10 w-full">products container</div>;
+  return <div className="bg-green-300 mx-5 w-full">products container</div>;
 };
 
 export default Products;
